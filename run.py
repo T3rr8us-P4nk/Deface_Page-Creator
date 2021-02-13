53f1d976e634ccdfdfee02ebe49bc3bb8648dc39
T3rr8us-P4nk <78995050+T3rr8us-P4nk@users.noreply.github.com>
2021-02-13 12:32:03 +0800

    Add files via upload

---
+++ run.py
@@ -0,0 +1,330 @@
+#Created By: T3rr8us P4nk
+#Thanks For Using This Tool
+import os
+import sys
+import time
+
+#Function
+def Writertext(z):
+	for e in z + '\n':
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(0.07)
+
+#Banner
+time.sleep(1)
+banner = """\033[96m
++=========================================+
+|...........Deface Page Creator...........|
++-----------------------------------------+
+|#Created By:     T3rr8us P4nk            |
+|#Contact: exploitcrew091@gmail.com       |
+|#Version:         2.0.11                 |
+|#Thanks To: Ph.Kury, Mr.C0dex, Clemente  |
++=========================================+
+|...........Deface Page Creator...........|
++-----------------------------------------+
+
+
+\033[1;31;40mYou Can Skip All Question If You Want To Skip
+Thanks For Using The Tool :)"""
+Writertext(banner)
+
+# Input Question
+print(" ")
+print(" ")
+title = input("\033[1;36;40m[+] Enter Deface Page Title : ")
+print(" ")
+time.sleep(1)
+Hacker_CN = input("\033[1;36;40m[+] Enter Hacker Code Name : ")
+print(" ")
+time.sleep(1)
+message = input("\033[1;36;40m[+] Enter Your Message On The Website (You Can Use <br> To Step Line Under) : ")
+print(" ")
+time.sleep(1)
+choice = input("\033[1;36;40m[+] Do You Want To Continue [y/n] : ")
+
+# Source Code
+code1 = """<html>
+<head>
+<title>"""
+
+code2 = title
+
+code3 = """</title>
+</head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Hacked By T3rr8us P4nk">
+    <meta name="author" content="T3rr8us">
+
+    <!-- Le styles -->
+	<link rel="SHORTCUT ICON" href="http://winhacker.do.am/favicon.ico" />
+	<link href="https://fonts.googleapis.com/css2?family=Iceland&display=swap" rel="stylesheet">
+<style>
+
+    /* GLOBAL STYLES
+    -------------------------------------------------- */
+    /* Padding below the footer and lighter body text */
+
+    body {
+      background: #000;
+      padding-bottom: 40px;
+      color: #000;
+      
+      -webkit-transform: rotateX(90deg);
+      -moz-transform: rotateX(90deg);
+      -ms-transform: rotateX(90deg);
+      -o-transform: rotateX(90deg);
+      transform: rotateX(90deg);
+      -webkit-transform: translateZ(-100px);
+      -moz-transform: translateZ(-100px);
+      -ms-transform: translateZ(-100px);
+      -o-transform: translateZ(-100px);
+      transform: translateZ(-100px);
+      -webkit-animation: rain 3s infinite linear;
+      -moz-animation: rain 3s infinite linear;
+      -ms-animation: rain 3s infinite linear;
+      -o-animation: rain 3s infinite linear;
+      animation: rain 3s infinite linear;
+    }
+
+    @-webkit-keyframes rain {
+      from { background-position: left 0px, 40px 0px, left 0px; }
+      to   { background-position: left 200px, 40px 200px, left 300px; }
+    }
+    @-moz-keyframes rain {
+      from { background-position: left 0px, 40px 0px, left 0px; }
+      to   { background-position: left 200px, 40px 200px, left 300px; }
+    }
+    @-ms-keyframes rain {
+      from { background-position: left 0px, 40px 0px, left 0px; }
+      to   { background-position: left 200px, 40px 200px, left 300px; }
+    }
+    @-o-keyframes rain {
+      from { background-position: left 0px, 40px 0px, left 0px; }
+      to   { background-position: left 200px, 40px 200px, left 300px; }
+    }
+    @keyframes rain {
+      from { background-position: left 0px, 40px 0px, left 0px; }
+      to   { background-position: left 200px, 40px 200px, left 300px; }
+    }
+
+
+    /* Featurettes
+    ------------------------- */
+
+    .featurette-divider {
+      margin: 80px 0; /* Space out the Bootstrap <hr> more */
+    }
+    .featurette {
+      padding-top: 0px; /* Vertically center images part 1: add padding above and below text. */
+      overflow: hidden; /* Vertically center images part 2: clear their floats. */
+    }
+    .featurette-image {
+      margin-top: -120px; /* Vertically center images part 3: negative margin up the image the same amount of the padding to center it. */
+    }
+
+    /* Give some space on the sides of the floated elements so text doesn't run right into it. */
+    .featurette-image.pull-left {
+      margin-right: 40px;
+    }
+    .featurette-image.pull-right {
+      display: block;
+        float: none;
+        max-width: 40%;
+        margin: 0 auto 20px;
+    }
+
+    /* Thin out the marketing headings */
+    .featurette-heading {
+      font-size: 50px;
+      font-weight: 300;
+      line-height: 1;
+      letter-spacing: -1px;
+	  
+	  font-family: Verdana, Arial, Helvetica, sans-serif; 
+	  color : black;
+    }
+
+
+
+    /* RESPONSIVE CSS
+    -------------------------------------------------- */
+
+    @media (max-width: 979px) {
+
+      .featurette {
+        height: auto;
+        padding: 0;
+      }
+	  
+      .featurette-image.pull-left,
+      .featurette-image.pull-right {
+        display: block;
+        float: none;
+        max-width: 40%;
+        margin: 0 auto 20px;
+      }
+    }
+
+
+    @media (max-width: 767px) {
+
+      .featurette-heading {
+        font-size: 30px;
+      }
+      .featurette .lead {
+        font-size: 15px;
+        line-height: 1.5;
+      }
+
+    }
+    </style>	
+    <body>
+<!--
+ * Do not Copy 
+ * @author Winhacker
+ * @copyright 2016 Winhacker
+------------------------------------------------------- 
+-->
+	<br><br><div class="featurette">
+		<img src=https://1.bp.blogspot.com/-lkZ9ux3dr9s/YCJY3a6oqvI/AAAAAAAAAKY/T1PCtJ0Q5bAdGEfkW5qYwsdhMIVDmJ_4ACLcBGAsYHQ/s720/FB_IMG_1612583197485.jpg width=99% height=200px>
+		<center><h2 class="featurette-heading"><b>
+		<script> 
+		farbbibliothek = new Array(); 
+		farbbibliothek[0] = new Array("#FF0000","#FF1100","#FF2200","#FF3300","#FF4400","#FF5500","#FF6600","#FF7700","#FF8800","#FF9900","#FFaa00","#FFbb00","#FFcc00","#FFdd00","#FFee00","#FFff00","#FFee00","#FFdd00","#FFcc00","#FFbb00","#FFaa00","#FF9900","#FF8800","#FF7700","#FF6600","#FF5500","#FF4400","#FF3300","#FF2200","#FF1100"); 
+		farbbibliothek[1] = new Array("#00FF00","#000000","#00FF00","#00FF00"); 
+		farbbibliothek[2] = new Array("#00FF00","#FF0000","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00","#00FF00"); 
+		farbbibliothek[3] = new Array("#FF0000","#FF4000","#FF8000","#FFC000","#FFFF00","#C0FF00","#80FF00","#40FF00","#00FF00","#00FF40","#00FF80","#00FFC0","#00FFFF","#00C0FF","#0080FF","#0040FF","#0000FF","#4000FF","#8000FF","#C000FF","#FF00FF","#FF00C0","#FF0080","#FF0040"); 
+		farbbibliothek[4] = new Array("#FF0000","#EE0000","#DD0000","#CC0000","#BB0000","#AA0000","#990000","#880000","#770000","#660000","#550000","#440000","#330000","#220000","#110000","#000000","#110000","#220000","#330000","#440000","#550000","#660000","#770000","#880000","#990000","#AA0000","#BB0000","#CC0000","#DD0000","#EE0000"); 
+		farbbibliothek[5] = new Array("#000000","#000000","#000000","#FFFFFF","#FFFFFF","#FFFFFF"); 
+		farbbibliothek[6] = new Array("#0000FF","#FFFF00"); 
+		farben = farbbibliothek[4];
+		function farbschrift() 
+		{ 
+			for(var i=0 ; i<Buchstabe.length; i++) 
+			{ 
+				document.all["a"+i].style.color=farben[i]; 
+			} 
+			farbverlauf(); 
+		} 
+		function string2array(text) 
+		{ 
+			Buchstabe = new Array(); 
+			while(farben.length<text.length) 
+			{ 
+				farben = farben.concat(farben); 
+			} 
+			k=0; 
+			while(k<=text.length) 
+			{ 
+				Buchstabe[k] = text.charAt(k); 
+				k++; 
+			} 
+		} 
+		function divserzeugen() 
+		{ 
+			for(var i=0 ; i<Buchstabe.length; i++) 
+			{ 
+				document.write("<span id='a"+i+"' class='a"+i+"'>"+Buchstabe[i] + "</span>"); 
+			} 
+			farbschrift(); 
+		} 
+		var a=1; 
+		function farbverlauf() 
+		{ 
+			for(var i=0 ; i<farben.length; i++) 
+			{ 
+				farben[i-1]=farben[i]; 
+			} 
+			farben[farben.length-1]=farben[-1]; 
+ 
+			setTimeout("farbschrift()",30); 
+		} 
+		// Zu Demonstrationszwecken***************** 
+		var farbsatz=1; 
+		function farbtauscher() 
+		{ 
+			farben = farbbibliothek[farbsatz]; 
+			while(farben.length<text.length) 
+			{ 
+				farben = farben.concat(farben); 
+			} 	
+			farbsatz=Math.floor(Math.random()*(farbbibliothek.length-0.0001)); 
+		} 
+		setInterval("farbtauscher()",4500); 
+		text=" You Have Been Hacked ";
+//h 
+		string2array(text);
+		divserzeugen(); 
+//document.write(text);   
+//
+/*function expand() {
+for(x = 0; x < 50; x++) {
+window.moveTo(screen.availWidth * -(x - 50) / 100, screen.availHeight * -(x - 50) / 100);
+window.resizeTo(screen.availWidth * x / 50, screen.availHeight * x / 50);
+}
+window.moveTo(0,0);
+window.resizeTo(screen.availWidth, screen.availHeight);
+}
+expand();*/
+	</script>"""
+
+code4 = """</b></h2></center>
+		<hr width="80%" style="color:black" />
+
+		<div style="background : black; padding : 10px;">
+<b><center>
+<font face="Tahoma" size="5" color=red>"""
+
+
+
+
+code5 = """[<marquee style="width:200px;height:26px;"><font face="Iceland" color=white>Hacked By : <font face="Iceland" color=red>"""
+
+code6 = Hacker_CN
+
+code7 = """<font color=red></font></font></marquee>]</font></center></b>
+		</div>"""
+
+code8 = """
+<p style="font-family: Verdana, Arial, Helvetica, sans-serif; font-size : 16px;" align="center"><font style="font-family: Verdana, Arial, Helvetica, sans-serif; color : red; font-size : 16px;" align="center">Hello <font style="font-family: Verdana, Arial, Helvetica, sans-serif; color : white; font-size : 16px;" align="center">"""
+
+code9 = message 
+
+code10 = """</p>
+    </div>
+	<br/><br/>
+
+  </body>
+</html>"""
+
+if choice == "y":
+	time.sleep(3)
+	os.system("clear")
+	Writertext("\033[1;32;40mGenerating Your Deface Page Please Wait A Second :)")
+#Open the index
+	fo = open("deface-page.html","w")
+	time.sleep(5)
+elif choice == "n":
+	exit()
+
+fo.write(code1)
+fo.write(code2)
+fo.write(code3)
+fo.write(code4)
+fo.write(code5)
+fo.write(code6)
+fo.write(code7)
+fo.write(code8)
+fo.write(code9)
+fo.write(code10)
+
+print(" ")
+print ("""The  page has been generated as: deface-page.html.
+
+There Are Some Items That Have Been Added On Your Deface Page But Don't Worry You Can Edit It On Your Html Editor :)
+
+Type : cp -r deface-page.html /sdcard""")
+
+fo.close()
